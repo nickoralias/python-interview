4bc2594d4f0c2117d671d102d4744fdef5afb376
Nick Lofaro <nicholas@cabansystems.com>
2020-03-19 23:19:47 -0700

    4.1 solution

---
+++ primitive_types/4_1.py
@@ -0,0 +1,12 @@
+def parity(x):
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 0x1
+
+if __name__ == '__main__':
+    print(parity(8))
+    print(parity(3))
